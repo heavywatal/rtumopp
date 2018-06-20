da6d36bc335451f679edb9ed4a8bda0b8dc78743
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-06-20 16:05:31 +0900

    :boom: Separate ms-like functions

--- R/ms.R
+++ R/ms.R
@@ -1,36 +1,22 @@
-#' Make ms-like neutral variation matrix
-#' @param population tibble of raw population
-#' @param nsam number of cells to sample
+#' Sprinkle mutations on genealogy
+#'
+#' `mutate_clades` samples nodes to mutate and returns their descendants
+#' @param graph igraph
 #' @param mu mutation rate per cell division (ignored if segsites is given)
 #' @param segsites number of segregating sites
-#' @return int matrix (nsam x segsites)
 #' @rdname ms
 #' @export
-sprinkle_mutations = function(population, nsam, mu=NULL, segsites=NULL) {
-  graph = make_igraph(population)
-  samples = sample(filter_extant(population)$id, nsam) %>% as.character()
-  lineages = igraph::neighborhood(graph, order = 1073741824L, nodes = samples, mode = "in") %>%
-    purrr::map(~ .x$name)
-  common_ancs = purrr::reduce(lineages, intersect)
-  nodes = purrr::flatten_chr(lineages) %>% unique() %>% setdiff(common_ancs)
+mutate_clades = function(graph, mu=NULL, segsites=NULL) {
+  nodes = names(igraph::V(graph))
+  nodes = nodes[igraph::degree(graph, mode = "out") > 0L]
+  # remove singletons
+  # TODO: remove low-freq variants?
   if (is.null(segsites)) {
     if (is.null(mu)) stop("specify either mu or segsites")
     segsites = stats::rpois(1L, length(nodes) * mu)
   } else if (!is.null(mu)) warning("mu is ignored if segsites is given")
-  mutant_ids = sample(nodes, segsites, replace = TRUE) %>% sort()
-  lineages %>%
-    purrr::map(~ mutant_ids %in% .) %>%
-    purrr::flatten_int() %>%
-    matrix(nrow = nsam, byrow = TRUE)
-}
-
-#' Summary statistics of neutral mutations
-#' @param msout tibble from `sprinkle_mutations()`
-#' @param lowerbound to discard peripheral branches
-#' @return tibble
-#' @rdname ms
-#' @export
-sample_stats = function(msout, lowerbound=0) {
-  vaf = colMeans(msout)
-  tibble::tibble(math = math_score(vaf[vaf > lowerbound]))
+  mutants = sample(nodes, segsites, replace = TRUE)
+  igraph::ego(graph, order=1073741824L, mutants, mode = "out") %>%
+    # TODO: remove internal nodes?
+    purrr::map(names)
 }
---
+++ R/vaf.R
@@ -0,0 +1,27 @@
+#' Utilities for variant allele frequency
+#'
+#' @description
+#' `tally_vaf` evaluates overlap of sampled and mutated cells
+#' @param samples list of integer IDs
+#' @param sites list of integer IDs
+#' @rdname vaf
+#' @export
+tally_vaf = function(samples, sites) {
+  purrr::map_dfc(samples, function(region) {
+    purrr::map_int(sites, function(holders) {
+      sum(region %in% holders)
+    }) / length(region)
+  })
+}
+
+#' `tidy_vaf` transforms vaf table
+#' @param tbl output of `tally_vaf`
+#' @rdname vaf
+#' @export
+tidy_vaf = function(tbl) {
+  tbl %>%
+    rlang::set_names(seq_len(ncol(.))) %>%
+    tibble::rowid_to_column(var = "site") %>%
+    tidyr::gather("sample", "frequency", -"site") %>%
+    dplyr::mutate(sample = as.integer(.data$sample))
+}
