af84ac937d998210147804c7e6b928f899628d0d
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-06-21 18:46:56 +0900

    :sparkles: Add sort_vaf()

--- R/vaf.R
+++ R/vaf.R
@@ -26,3 +26,28 @@ tidy_vaf = function(tbl) {
     dplyr::filter(.data$frequency > 0) %>%
     dplyr::mutate(sample = as.integer(.data$sample))
 }
+
+#' `sort_vaf` reorders rows and columns of VAF table
+#' @param method passed to `stats::hclust`
+#' @rdname vaf
+#' @export
+sort_vaf = function(tbl, method = c("average", "ward.D2", "complete", "single")) {
+  method = match.arg(method)
+  if (nrow(tbl) < 2L) return(tbl)
+  rsums = rowSums(tbl > 0)
+  w_nonzero = 10
+  w_shared = ifelse(rsums < 2L, 0, 1000)
+  tbl_weighted = dplyr::mutate_all(tbl, ~{
+    ifelse(.x > 0, w_nonzero, 0) + w_shared + .x
+  })
+  d_rows = stats::dist(tbl_weighted, method = "euclidean")
+  order_rows = stats::hclust(d_rows, method = method)$order
+  tbl_shared = dplyr::filter(tbl_weighted, rsums > 1L)
+  if (nrow(tbl_shared) > 1L) {
+    d_cols = stats::dist(t(tbl_shared), method = "euclidean")
+    order_cols = stats::hclust(d_cols, method = method)$order
+    tbl[order_rows, order_cols]
+  } else {
+    tbl[order_rows,]
+  }
+}
