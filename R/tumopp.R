8c77b1f7cf114b5c59a895a4456f21c6b492e28e
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-08-08 23:57:02 +0900

    :art: Use S3 method to switch tumopp()

--- R/tumopp.R
+++ R/tumopp.R
@@ -1,47 +1,56 @@
 #' Run tumopp
 #'
 #' `tumopp()` returns full results with config columns in a data.frame.
-#' @param args command line arguments as a string vector or list of strings
-#' @param nsam number of samples for ms-like output
+#' @param args command line arguments as a string vector or list of strings.
+#' @param ... not used.
 #' @export
-tumopp = function(args = character(0L), nsam = 0L) {
-  if (is.list(args)) {
-    purrr::map_dfr(args, tumopp, .id = "args")
-  } else {
-    if (length(args) == 1L) {
-      args = stringr::str_split(args, "\\s+") %>% purrr::flatten_chr()
-    }
-    message(paste(args, collapse = " "))
-    nrep = as.integer(nsam > 0L)
-    result = cpp_tumopp(c(nsam, nrep, args))
-    if (length(result) == 0L) return(invisible(NULL))
-    .out = wtl::read_boost_ini(result["config"])
-    .pop = read_tumopp(result["specimens"])
-    .snapshots = read_tumopp(result["snapshots"])
-    if ((.out$coord == "hex") && getOption("tumopp.autohex", TRUE)) {
-      .pop = trans_coord_hex(.pop)
-      .snapshots = trans_coord_hex(.snapshots)
-    }
-    .out = .out %>% dplyr::mutate(
-      population = list(.pop),
-      graph = list(make_igraph(.pop))
-    )
-    if (nrow(.snapshots) > 0L) {
-      .out = .out %>% dplyr::mutate(snapshots = list(.snapshots))
-    }
-    .drivers = readr::read_tsv(result["drivers"])
-    if (nrow(.drivers) > 0L) {
-      .out = .out %>% dplyr::mutate(drivers = list(.drivers))
-    }
-    .dist = readr::read_tsv(result["distances"])
-    if (nrow(.dist) > 0L) {
-      .out = .out %>% dplyr::mutate(distances = list(.dist))
-    }
-    if (nsam > 0L) {
-      .out$ms = wtl::parse_ms(strsplit(result["ms"], "\n")[[1L]])
-    }
-    .out
+tumopp = function(args, ...) UseMethod("tumopp")
+
+#' @param nsam number of samples for ms-like output.
+#' @rdname tumopp
+#' @export
+tumopp.default = function(args = character(0L), nsam = 0L, ...) {
+  if (length(args) == 1L) {
+    args = stringr::str_split(args, "\\s+") %>% purrr::flatten_chr()
   }
+  message(paste(args, collapse = " "))
+  nrep = as.integer(nsam > 0L)
+  result = cpp_tumopp(c(nsam, nrep, args))
+  if (length(result) == 0L) return(invisible(NULL))
+  .out = wtl::read_boost_ini(result["config"])
+  .pop = read_tumopp(result["specimens"])
+  .snapshots = read_tumopp(result["snapshots"])
+  if ((.out$coord == "hex") && getOption("tumopp.autohex", TRUE)) {
+    .pop = trans_coord_hex(.pop)
+    .snapshots = trans_coord_hex(.snapshots)
+  }
+  .out = .out %>% dplyr::mutate(
+    population = list(.pop),
+    graph = list(make_igraph(.pop))
+  )
+  if (nrow(.snapshots) > 0L) {
+    .out = .out %>% dplyr::mutate(snapshots = list(.snapshots))
+  }
+  .drivers = readr::read_tsv(result["drivers"])
+  if (nrow(.drivers) > 0L) {
+    .out = .out %>% dplyr::mutate(drivers = list(.drivers))
+  }
+  .dist = readr::read_tsv(result["distances"])
+  if (nrow(.dist) > 0L) {
+    .out = .out %>% dplyr::mutate(distances = list(.dist))
+  }
+  if (nsam > 0L) {
+    .out$ms = wtl::parse_ms(strsplit(result["ms"], "\n")[[1L]])
+  }
+  .out
+}
+
+#' @param mc.cores The number of cores to use for concurrent execution.
+#' @rdname tumopp
+#' @export
+tumopp.list = function(args, nsam = 0L, ..., mc.cores = 1L) {
+  parallel::mclapply(args, tumopp, nsam = nsam) %>%
+    dplyr::bind_rows(.id = "args")
 }
 
 #' @description
@@ -54,9 +63,9 @@ mslike = function(nsam = 20L, args = character(0L)) {
 
 #' @description
 #' `make_args()` returns argument combinations in a list.
-#' @param alt named list of altered arguments
-#' @param const unnamed vector of constant arguments
-#' @param nreps number of repeats
+#' @param alt named list of altered arguments.
+#' @param const unnamed vector of constant arguments.
+#' @param nreps number of repeats.
 #' @rdname tumopp
 #' @export
 make_args = function(alt, const = NULL, nreps = 1L) {
