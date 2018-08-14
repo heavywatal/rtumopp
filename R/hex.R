1be42c5143146e69243a729265dc60e4292aef45
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-08-14 15:38:29 +0900

    :sparkles: Add functions to revert hex transformation

--- R/hex.R
+++ R/hex.R
@@ -10,20 +10,48 @@ trans_coord_hex = function(.tbl) {
 
 # face centered cubic, cubic close packed
 trans_coord_fcc = function(.tbl) {
-  trans_coord_hex_xy(.tbl) %>%
-    dplyr::mutate(x = .data$x + .data$z / sqrt(3)) %>%
-    dplyr::mutate(z = .data$z * sqrt(2 / 3))
+  dplyr::mutate(
+    trans_coord_hex_xy(.tbl),
+    x = .data$x + .data$z / sqrt(3),
+    z = .data$z * sqrt(2 / 3)
+  )
 }
 
 # hexagonal close packed
 trans_coord_hcc = function(.tbl) {
-  trans_coord_hex_xy(.tbl) %>%
-    dplyr::mutate(x = .data$x + ifelse(.data$z %% 2L == 1L, sqrt(3) / 3, 0)) %>%
-    dplyr::mutate(z = .data$z * sqrt(2 / 3))
+  dplyr::mutate(
+    trans_coord_hex_xy(.tbl),
+    x = .data$x + ifelse(.data$z %% 2L == 1L, sqrt(3) / 3, 0),
+    z = .data$z * sqrt(2 / 3)
+  )
 }
 
 # 2D transformation into hexagonal grid
 trans_coord_hex_xy = function(.tbl) {
-  dplyr::mutate(.tbl, y = .data$y + .data$x * 0.5) %>%
-    dplyr::mutate(x = .data$x * sqrt(3 / 4))
+  dplyr::mutate(
+    .tbl,
+    y = .data$y + .data$x * 0.5,
+    x = .data$x * sqrt(3 / 4)
+  )
+}
+
+revert_coord_hex = function(.tbl) {
+  if (nrow(.tbl) == 0L) return(.tbl)
+  revert_coord_fcc(.tbl)
+}
+
+revert_coord_fcc = function(.tbl) {
+  dplyr::mutate(
+    .tbl,
+    z = round(.data$z * sqrt(3 / 2)),
+    x = round(.data$x - .data$z / sqrt(3))
+  ) %>% revert_coord_hex_xy()
+}
+
+revert_coord_hex_xy = function(.tbl) {
+  dplyr::mutate(
+    .tbl,
+    x = round(.data$x * sqrt(4 / 3)),
+    y = round(.data$y - .data$x * 0.5)
+  )
 }
