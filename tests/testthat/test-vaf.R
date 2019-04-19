441738365144c2fc9a63cc8917c95e6cc7ea7ef4
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-04-19 12:06:15 +0900

    :white_check_mark: Add graph-related tests

--- tests/testthat/test-vaf.R
+++ tests/testthat/test-vaf.R
@@ -1,16 +1,30 @@
 context("test-vaf")
 
 test_that("make_vaf works", {
-  result = tumopp("-N1024")
+  result = tumopp("--seed=42 -N256 -k1e6")
   population = result$population[[1L]]
   extant = filter_extant(population)
   graph = make_igraph(population)
+  nsam = 4L
+  ncell = 4L
   .colnames = c("x", "y", "z", "id")
   expect_named({
-    regions = sample_uniform_regions(extant, nsam = 4L, ncell = 4L)
+    regions = sample_uniform_regions(extant, nsam = nsam, ncell = ncell)
   }, .colnames)
   .colnames = c("site", "sample", "frequency")
   expect_named({
     vaf = make_vaf(graph, regions$id, mu = 1)
   }, .colnames)
+  sampled = purrr::flatten_int(regions$id)
+  expect_silent({
+    subgraph = subtree(graph, sampled)
+  })
+  within_between_samples(graph, regions)
+  expect_silent({
+    df_distances = within_between_samples(subgraph, regions)
+  })
+  expect_named(df_distances, c("region_i", "region_j", "within_i", "within_j", "euclidean", "between", "within", "fst"))
+  expect_equal(nrow(df_distances), choose(nsam, 2L))
+  expect_equal(df_distances, within_between_samples(graph, regions))
+  expect_type(internal_nodes(subgraph, sampled, sensitivity = 0.10), "integer")
 })
